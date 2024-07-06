2b7605772c75a5544286a613eb824ebf03da3238
Maureen Ambasa <ambasaeli@gmail.com>
2024-07-05 18:42:09 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+/**
+ * print_alphabet_reverse - Recursively prints the lowercase alphabet in reverse
+ * @ch: Current character to print
+ */
+void print_alphabet_reverse(char ch)
+{
+	if (ch >= 'a')
+	{
+		putchar(ch);           // Print the current character
+		print_alphabet_reverse(ch - 1); // Recursively call with the previous character
+	}
+}
 
 /**
  * main - Entry point
  *
+ * Description: Prints the lowercase alphabet in reverse
+ * followed by a new line using only two putchar calls
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
-
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
+	print_alphabet_reverse('z');
+	putchar('\n');
 
 	return (0);
 }
