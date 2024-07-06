87a441dd9565bf726fe73b835951e3325553b129
Maureen Ambasa <ambasaeli@gmail.com>
2024-07-05 19:24:22 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of single-digit numbers
- *              separated by ", " in ascending order using putchar
- *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i, j;
+	int d;
 
-    /* Loop through tens place digits */
-    for (i = 0; i < 10; i++)
-    {
-        /* Loop through ones place digits */
-        for (j = 0; j < 10; j++)
-        {
-            /* Print the current combination */
-            putchar(i + '0'); /* Print tens place digit */
-            putchar(j + '0'); /* Print ones place digit */
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-            /* Check if it's not the last combination */
-            if (!(i == 9 && j == 9))
-            {
-                putchar(','); /* Print comma */
-                putchar(' '); /* Print space */
-            }
-        }
-    }
-
-    putchar('\n'); /* Print newline */
-
-    return (0);
+	return (0);
 }
