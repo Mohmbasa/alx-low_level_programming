8de801bc8859ec2bfbb9834bc129f40e655830e0
Maureen Ambasa <ambasaeli@gmail.com>
2024-07-05 18:51:37 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,25 +6,25 @@
  */
 void print_alphabet_reverse(char ch)
 {
-	if (ch >= 'a')
-	{
-		putchar(ch);           /* Print the current character */
-		print_alphabet_reverse(ch - 1); /* Recursively call with the previous character */
-	}
+    if (ch >= 'a')
+    {
+        putchar(ch);                    /* Print the current character */
+        print_alphabet_reverse(ch - 1); /* Recursively call with the previous character */
+    }
 }
 
 /**
  * main - Entry point
  *
  * Description: Prints the lowercase alphabet in reverse
- * followed by a new line using only two putchar calls
+ *              followed by a new line using only two putchar calls
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print_alphabet_reverse('z');
-	putchar('\n');
+    print_alphabet_reverse('z');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
