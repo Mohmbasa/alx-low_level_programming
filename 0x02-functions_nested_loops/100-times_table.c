8f91fa26b7c553e9fe17ca475441cfbbd13ee6b5
Maureen Ambasa <ambasaeli@gmail.com>
2024-07-07 07:45:30 -0700

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,15 +12,16 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
-	for (int i = 0; i <= n; i++)
+	int i, j;
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int result = i * j;
 			if (j == 0)
 				printf("%d", result);
 			else
-				printf("%4d", result); // Adjust spacing for alignment
+				printf("%4d", result); /* Adjust spacing for alignment */
 			if (j < n)
 				printf(",");
 		}
