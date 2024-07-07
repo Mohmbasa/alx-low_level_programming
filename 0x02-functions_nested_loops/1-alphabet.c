4b6b72f1d826633fc8e472a50e12107d145dfa74
Maureen Ambasa <ambasaeli@gmail.com>
2024-07-07 06:33:40 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,18 +5,28 @@
  */
 void print_alphabet_x10(void)
 {
+	/* Initialize loop counter */
 	int i = 0;
+	/* Declare variable for alphabet letter */
 	char letter;
 
+	/* Loop through 10 times */
 	while (i < 10)
 	{
+		/* Reset letter to 'a' at the start of each iteration */
 		letter = 'a';
+
+		/* Print each letter from 'a' to 'z' */
 		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
+
+		/* Print newline character after printing each set of alphabet */
 		_putchar('\n');
+
+		/* Increment loop counter */
 		i++;
 	}
 }
